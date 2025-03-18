768dfc2cc3327622ef82d3e34eb642a7ac0ac088
Silvana <sbromero@fi.uba.ar>
2025-03-17 23:56:16 -0300

    Primer commit

---
+++ Arrays.js
@@ -0,0 +1,19 @@
+// Ejercicios Arrays
+
+//1-1️⃣ Agregar y quitar elementos
+//Crea un array llamado animales con tres nombres de animales. Luego:
+
+//Agrega un nuevo animal al inicio.
+//Agrega otro al final.
+//Elimina el primer animal.
+//Muestra el array final en la consola.
+
+
+animales = ["Gato", "Perro", "Lobo"]
+console.log(animales);
+animales.unshift("Cocodrilo");
+console.log(animales);
+animales.push("Pajaro");
+console.log(animales);
+animales.shift();
+console.log(animales);
